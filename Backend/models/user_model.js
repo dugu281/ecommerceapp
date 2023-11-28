4fa62ed3689e21153dbe4bc785bdbe2ad6c2c252
dugu281 <102406954+dugu281@users.noreply.github.com>
2023-11-28 12:33:08 +0530

    Update user_model.js

--- Backend/models/user_model.js
+++ Backend/models/user_model.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     },
     isAdmin: {
         type: Boolean,
-        default: true,
+        default: false,
         required: true
     },
     phone: {
